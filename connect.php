480a4c0cd0381e94120063bb8cb40ef72c005903
kiranKverma <kiranjeet5620@gmail.com>
2020-02-05 14:34:52 -0500

    --- Auto-Git Commit ---

--- connect.php
+++ connect.php
@@ -1,9 +1,42 @@
 <?php
-$connection = mysqli_connect('localhost','root', '');
-if (!$connection){
-    die("Database Connection Failed".mysqli_error($connection));
+$email = $_POST['email'];
+$password=$_POST['password'];
+$cpassword=$_POST['cpassword'];
+$fname = $_POST['fname'];
+$lname=$_POST['lname'];
+$dob=$_POST['dob'];
+if (!empty($email) || !empty($password) || !empty($cpassword) || !empty($fname) || !empty($lname) || !empty($dob)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "photographymanagement";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $INSERT = "INSERT Into user (email, password, cpassword, fname, lname, dob) values(?, ?, ?, ?, ?, ?)";
+     //Prepare statement
+    
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("bb", $email, $password, $cpassword, $fname, $lname, $dob);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
 }
-$select_db = mysqli_select_db($connection,'photographymanagement');
-if (!$select_db){
-    die("Database Selection Failed".mysqli_error($connection));
-}
+?>
