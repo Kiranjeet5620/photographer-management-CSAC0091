dd1a4bc77f7baf7d80984e0a056ef653fc61c9be
kiranKverma <kiranjeet5620@gmail.com>
2020-02-05 23:28:31 -0500

    --- Auto-Git Commit ---

--- connect.php
+++ connect.php
@@ -1,42 +1,36 @@
-<?php
-$email = $_POST['email'];
-$password=$_POST['password'];
-$cpassword=$_POST['cpassword'];
-$fname = $_POST['fname'];
-$lname=$_POST['lname'];
-$dob=$_POST['dob'];
-if (!empty($email) || !empty($password) || !empty($cpassword) || !empty($fname) || !empty($lname) || !empty($dob)) {
- $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "photographymanagement";
-    //create connection
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-    if (mysqli_connect_error()) {
-     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-    } else {
-     $INSERT = "INSERT Into user (email, password, cpassword, fname, lname, dob) values(?, ?, ?, ?, ?, ?)";
-     //Prepare statement
-    
-     $stmt->bind_param("s", $email);
-     $stmt->execute();
-     $stmt->bind_result($email);
-     $stmt->store_result();
-     $rnum = $stmt->num_rows;
-     if ($rnum==0) {
-      $stmt->close();
-      $stmt = $conn->prepare($INSERT);
-      $stmt->bind_param("bb", $email, $password, $cpassword, $fname, $lname, $dob);
-      $stmt->execute();
-      echo "New record inserted sucessfully";
-     } else {
-      echo "Someone already register using this email";
-     }
-     $stmt->close();
-     $conn->close();
-    }
-} else {
- echo "All field are required";
- die();
-}
-?>
+<!DOCTYPE HTML>
+<html>
+<body>
+<?php 
+ include("config.php"); 
+ session_start(); //always start a session in the beginning 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{ 
+ if (empty($_POST['username']) || empty($_POST['password'])) //Validating inputs using PHP code 
+ { 
+ echo "Incorrect username or password"; //
+ header("location:login.html");//You will be sent to Login.php for re-login 
+ } 
+ 
+ $username = $_POST["username"]; // as the method type in the form is "post" we are using $_POST otherwise it would be $_GET[] 
+ $password = $_POST["password"]; 
+ $stmt= $db->prepare("SELECT useranme, password FROM login WHERE username = ?"); //Fetching all the records with input credentials
+ $stmt->bind_param("s", $username); //bind_param() - Binds variables to a prepared statement as parameters. "s" indicates the type of the parameter.
+ $stmt->execute();
+ $stmt->bind_result($UsernameDB, $PasswordDB); // Binding i.e. mapping database results to new variables
+   
+ //Compare if the database has username and password entered by the user. Password has to be decrypted while comparing.
+ if ($stmt->fetch() && password_verify($password, $PasswordDB)) 
+ {
+ $_SESSION['username']=$username; //Storing the username value in session variable so that it can be retrieved on other pages
+ header("location: admin.html"); // user will be taken to profile page
+ }
+ else
+ {
+    echo "Incorrect username or password"; 
+   
+ } 
+ } 
+       ?>
+ </body> 
+ </html>
