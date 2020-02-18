fb33800048429e18d7a5459aa7c5874447901189
kiranKverma <kiranjeet5620@gmail.com>
2020-02-18 14:56:01 -0500

    --- Auto-Git Commit ---

--- insert.php
+++ insert.php
@@ -1,43 +1,15 @@
-<?php
-$email = $_POST['email'];
-$pass = $_POST['password'];
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$dob = $_POST['dob'];
-if (!empty($email) || !empty($pass) || !empty($fname) || !empty($lname) || !empty($dob) ) {
-    $host = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbname = "photographymanagement";
-    //create connection
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-    if (mysqli_connect_error()) {
-     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-    } else {
-     $SELECT = "SELECT Email From user Where email = ? Limit 1";
-     $INSERT = "INSERT Into user (UserId,Email, Password, FirstName, LastName, Dob, AccessType,Phone,
-     Department,Address,PostalCode) values(?, ?, ?, ?, ?, ?,?,?,?,?,?)";
-     //Prepare statement
-     $stmt = $conn->prepare($SELECT);
-     $stmt->bind_param("s", $email);
-     $stmt->execute();
-     $stmt->bind_result($email);
-     $stmt->store_result();
-     $rnum = $stmt->num_rows;
-     if ($rnum==0) {
-      $stmt->close();
-      $stmt = $conn->prepare($INSERT);
-      $stmt->bind_param("ssssii", $email, $pass, $fname, $lname, $dob);
-      $stmt->execute();
-      echo "New record inserted sucessfully";
-     } else {
-      echo "Someone already register using this email";
-     }
-     $stmt->close();
-     $conn->close();
-    }
-} else {
- echo "All field are required";
- die();
-}
+<?php  
+include("config.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $dob = $_POST["dob"];
+    $sql="insert into  user(Email,Password,FirstName,LastName,Dob) Values('".$email. " ',
+    ' " .$pass. " ',' " . $fname. " ',' " .$lname. " ',' " .$dob. " ') ";  
+    $result = mysqli_query($conn,$sql);
+    echo "1 record added";
+  }
+  echo "errorrrrrrrrrr";
 ?>
