f5dae451cac371d8071c261ab04c2166047d43e8
kiranKverma <kiranjeet5620@gmail.com>
2020-02-18 13:13:16 -0500

    --- Auto-Git Commit ---

---
+++ insert.php
@@ -0,0 +1,14 @@
+<?php  
+include("config.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $dob = $_POST["dob"];
+    $sql="insert into  user(Email,Password,FirstName,LastName,Dob) Values('".$email. " ',
+    ' " .$pass. " ',' " . $fname. " ',' " .$lname. " ',' " .$dob. " ') ";  
+    $result = mysqli_query($conn,$sql);
+  }
+  
+?>
