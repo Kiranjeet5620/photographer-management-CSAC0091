d6a15fd938fbb87767ae920f6de2a1a0142d4fd5
kiranKverma <kiranjeet5620@gmail.com>
2020-02-19 14:36:58 -0500

    --- Auto-Git Commit ---

--- insert.php
+++ insert.php
@@ -2,40 +2,22 @@
 //Step1
 $db = mysqli_connect('localhost','root','','photographymanagement')
 or die('Error connecting to MySQL server.');
-if(isset($_POST['submit'])){
-    $email = $_POST["email"];
-    $pass = $_POST["password"];
-    $cpass=$_POST["cpassword"];
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $dob = $_POST["dob"];
 
-    $sql="insert into  signup(email,password,fname,lname,dob,cpass) Values('$email','$pass',' $fname','$lname','$dob','$cpass') ";  
+    $email = $_POST['email];
+    $pass = $_POST['password'];
+    $cpass=$_POST['cpassword'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $dob = $_POST['dob'];
+
+    $sql="insert into  signup (email,password,fname,lname,dob,cpass) Values('$email','$pass',' $fname','$lname','$dob','$cpass') ";  
     
                 if (mysqli_query($db, $sql)) {
-                  header("url=regularuser.html")
+                  echo "added 1 record";
               } else {
                   echo "Error: " . $sql . "<br>" . mysqli_error($db);
               }
-  }
-  else{
-  echo "errorrrrrrrrrr";
-  }
-  mysqli_close($db);
+  
+  header("refresh:2; url=signup.html");
 
 ?>
-<html>
- <head>
- </head>
- <body>
- <h1>zxdcfgvbh</h1>
-</body>
-</html>
-
-<html>
- <head>
- </head>
- <body>
- <h1>PHP connect to MySQL scmmcslmkdcms</h1>
-</body>
-</html>
