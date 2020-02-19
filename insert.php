8f9216fddc50608cb494ef1113db66e0181fe6db
kiranKverma <kiranjeet5620@gmail.com>
2020-02-19 16:33:16 -0500

    --- Auto-Git Commit ---

--- insert.php
+++ insert.php
@@ -1,23 +1,23 @@
-<?php  
+<?php
 //Step1
-$db = mysqli_connect('localhost','root','','photographymanagement')
-or die('Error connecting to MySQL server.');
-
+ $db = mysqli_connect('localhost','root','','photographymanagement')
+ or die('Error connecting to MySQL server.');
+ if(isset($_POST['submit'])){
     $email = $_POST['email];
     $pass = $_POST['password'];
     $cpass=$_POST['cpassword'];
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $dob = $_POST['dob'];
-
-    $sql="insert into  signup (email,password,fname,lname,dob,cpass) Values('$email','$pass',' $fname','$lname','$dob','$cpass') ";  
-    
-                if (mysqli_query($db, $sql)) {
+ 
+    $sql="insert into signup (email,pass,cpass,fname,lname,dob) Values('$email','$pass','$cpass',' $fname','$lname','$dob') ";  
+    $query=(mysqli_query($db, $sql);
+                if ($query) {
                   echo "added 1 record";
               } else {
                   echo "Error: " . $sql . "<br>" . mysqli_error($db);
               }
   
-  header("refresh:2; url=signup.html");
-
+  header(Location: regularuser.html);
+  }
 ?>
