17a69f75bf83d697c1762261668e769fb75ea07b
kiranKverma <kiranjeet5620@gmail.com>
2020-02-19 13:36:46 -0500

    --- Auto-Git Commit ---

--- insert.php
+++ insert.php
@@ -1,14 +1,30 @@
 <?php  
-include("config.php");
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//Step1
+$db = mysqli_connect('localhost','root','','photographymanagement')
+or die('Error connecting to MySQL server.');
+if(isset($_POST['submit'])){
     $email = $_POST["email"];
     $pass = $_POST["password"];
+    $cpass=$_POST["cpassword"];
     $fname = $_POST["fname"];
     $lname = $_POST["lname"];
     $dob = $_POST["dob"];
-    $sql="insert into  user(Email,Password,FirstName,LastName,Dob) Values($email,$pass, $fname,$lname,$dob) ";  
-    $result = mysqli_query($conn,$sql);
+
+    $sql="insert into  signup(email,password,fname,lname,dob,cpass) Values($email,$pass, $fname,$lname,$dob,$cpass) ";  
+    $result = mysqli_query($db,$sql);
     echo "1 record added";
   }
+  else{
   echo "errorrrrrrrrrr";
+  }
+  mysqli_close($db);
+
 ?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL scmmcslmkdcms</h1>
+</body>
+</html>
