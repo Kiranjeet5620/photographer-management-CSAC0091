db2a121611e28f0575564e96d59c6ada4dbda3c9
kiranKverma <kiranjeet5620@gmail.com>
2020-02-19 21:56:03 -0500

    --- Auto-Git Commit ---

--- insert.php
+++ insert.php
@@ -1,23 +1,27 @@
 <?php
 //Step1
- $db = mysqli_connect('localhost','root','','photographymanagement')
+$db = mysqli_connect('localhost','root','','photographymanagement')
  or die('Error connecting to MySQL server.');
- if(isset($_POST['submit'])){
-    $email = $_POST['email];
-    $pass = $_POST['password'];
-    $cpass=$_POST['cpassword'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $dob = $_POST['dob'];
- 
-    $sql="insert into signup (email,pass,cpass,fname,lname,dob) Values('$email','$pass','$cpass',' $fname','$lname','$dob') ";  
-    $query=(mysqli_query($db, $sql);
-                if ($query) {
-                  echo "added 1 record";
-              } else {
-                  echo "Error: " . $sql . "<br>" . mysqli_error($db);
-              }
-  
-  header(Location: regularuser.html);
+ if (!$db) {
+  echo 'Not connected to server';
+}
+if (!mysqli_select_db($db, 'photographymanagement')) {
+  echo 'Database not selected';
+}
+if (isset($_POST['submit'])) {
+
+  $email = $_POST['email'];
+  $pass = $_POST['password'];
+  $cpass = $_POST['cpassword'];
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $dob = $_POST['dob'];
+
+  $sql = "INSERT INTO signup (email,pass,cpass,fname,lname,dob) Values('$email','$pass','$cpass',' $fname','$lname','$dob') ";
+  $result = mysqli_query($db, $sql);
+  if (!empty($result)) {
+    echo " inserted";
+  } else {
+    echo "something went wrong";
   }
-?>
+}
